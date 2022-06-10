0620b828132a6b7e6c873fb266d4e2a400b259ee
Benidict Dulce <benidict.programmer@gmail.com>
2022-06-10 22:57:37 +0800

    feature: add data, domain, network and theme module. also adding dimen resources in theme module.

---
+++ theme/src/main/java/com/benidict/theme/resources/Dimens.kt
@@ -0,0 +1,72 @@
+package com.benidict.theme.resources
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * Dimens use for padding and margin
+ **/
+val material_resources_1dp = 1.dp
+val material_resources_2dp = 2.dp
+val material_resources_3dp = 3.dp
+val material_resources_4dp = 4.dp
+val material_resources_5dp = 5.dp
+val material_resources_6dp = 6.dp
+val material_resources_7dp = 7.dp
+val material_resources_8dp = 8.dp
+val material_resources_9dp = 9.dp
+val material_resources_10dp = 10.dp
+val material_resources_11dp = 11.dp
+val material_resources_12dp = 12.dp
+val material_resources_13dp = 13.dp
+val material_resources_14dp = 14.dp
+val material_resources_15dp = 15.dp
+val material_resources_16dp = 16.dp
+val material_resources_17dp = 17.dp
+val material_resources_18dp = 18.dp
+val material_resources_19dp = 19.dp
+val material_resources_20dp = 20.dp
+val material_resources_21dp = 21.dp
+val material_resources_22dp = 22.dp
+val material_resources_23dp = 23.dp
+val material_resources_24dp = 24.dp
+val material_resources_25dp = 25.dp
+val material_resources_26dp = 26.dp
+val material_resources_27dp = 27.dp
+val material_resources_28dp = 28.dp
+val material_resources_29dp = 29.dp
+val material_resources_30dp = 30.dp
+
+/**
+ * Dimens use for text size
+ **/
+val material_resources_1sp = 1.sp
+val material_resources_2sp = 2.sp
+val material_resources_3sp = 3.sp
+val material_resources_4sp = 4.sp
+val material_resources_5sp = 5.sp
+val material_resources_6sp = 6.sp
+val material_resources_7sp = 7.sp
+val material_resources_8sp = 8.sp
+val material_resources_9sp = 9.sp
+val material_resources_10sp = 10.sp
+val material_resources_11sp = 11.sp
+val material_resources_12sp = 12.sp
+val material_resources_13sp = 13.sp
+val material_resources_14sp = 14.sp
+val material_resources_15sp = 15.sp
+val material_resources_16sp = 16.sp
+val material_resources_17sp = 17.sp
+val material_resources_18sp = 18.sp
+val material_resources_19sp = 19.sp
+val material_resources_20sp = 20.sp
+val material_resources_21sp = 21.sp
+val material_resources_22sp = 22.sp
+val material_resources_23sp = 23.sp
+val material_resources_24sp = 24.sp
+val material_resources_25sp = 25.sp
+val material_resources_26sp = 26.sp
+val material_resources_27sp = 27.sp
+val material_resources_28sp = 28.sp
+val material_resources_29sp = 29.sp
+val material_resources_30sp = 30.sp
